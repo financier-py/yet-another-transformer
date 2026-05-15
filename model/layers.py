b5df1637193484334c35785844b2aaf97a573502
Financier <mir.mir.01.2023@gmail.com>
2026-05-16 01:38:33 +0300

    feat: add EncoderLayer and PositionalEncoding

--- model/layers.py
+++ model/layers.py
@@ -45,4 +45,65 @@ class MultiHeadAttention(nn.Module):
 
         batch_size, num_heads, seq_len, d_k = x.size()
         x = x.transpose(1, 2).reshape(batch_size, seq_len, self.d_model)
-        return self.W_o(x) # (batch_size, seq_len, d_model)
+        return self.W_o(x)  # (batch_size, seq_len, d_model)
+
+
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x: torch.Tensor):
+        res = F.gelu(self.fc1(x))
+        res = self.fc2(self.dropout(res))
+        return res
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model: int, num_heads: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.ffn = PositionWiseFeedForward(d_model, d_ff, dropout)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+
+        self.dropout1 = nn.Dropout(p=dropout)
+        self.dropout2 = nn.Dropout(p=dropout)
+
+    def forward(self, x, mask=None):
+        norm_x = self.norm1(x)
+        atten_out, _ = self.mha(norm_x, norm_x, norm_x, mask)
+
+        x = x + self.dropout1(atten_out)
+
+        norm_x2 = self.norm2(x)
+        ffn_out = self.ffn(norm_x2)
+
+        x = x + self.dropout2(ffn_out)
+
+        return x
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, max_seq_len: int):
+        super().__init__()
+
+        self.pe = torch.zeros(max_seq_len, d_model)
+        position = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)
+
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10_000.0) / d_model)
+        )
+
+        self.pe[:, 0::2] = torch.sin(position * div_term)
+        self.pe[:, 1::2] = torch.cos(position * div_term)
+
+        self.pe = self.pe.unsqueeze(0)
+        self.register_buffer("pe", self.pe)
+
+    def forward(self, x: torch.Tensor):
+        # x: (batch_size, seq_len, d_model)
+        return x + self.pe[:, : x.size(1)]
